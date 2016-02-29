291f589e8802c8e0ce9b280b9bc3aa89b8ca0ccb
Rajat Gupta <rajatgupta0889@gmail.com>
2016-02-29 13:45:08 +0530

    Add Project

---
+++ H2O/app/src/main/java/studioz/dance/h2o/com/h2o/YouTubeActivity.java
@@ -0,0 +1,13 @@
+package studioz.dance.h2o.com.h2o;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YouTubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you_tube);
+    }
+}
